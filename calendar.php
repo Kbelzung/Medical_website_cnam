c58794c6d963d3bb0e1577c0a2149c68b3a09c5e
Kevin Belzung <Belzung.kevin@gmail.com>
2021-12-06 17:56:09 +0100

    create calendar

---
+++ calendar.php
@@ -0,0 +1,92 @@
+<link rel="stylesheet" href="css/calendar.css">
+<div class="calendar">
+    <header id="calendar_header">
+      <div id="calendar_months">
+        <img id="calendar_months_arrow_left" class="calendar_arrow" src="resources/chevron-left.svg"></img>
+        <h1 id="calendar_months_text" class="calendar_header_text">Janvier</h1>
+        <img id="calendar_months_arrow_right" class="calendar_arrow" src="resources/chevron-right.svg"></img>
+      </div>
+      <div id="calendar_months">
+        <img id="calendar_years_arrow_left" class="calendar_arrow" src="resources/chevron-left.svg"></img>
+        <h1 id="calendar_years_text" class="calendar_header_text">2021</h1>
+        <img id="calendar_years_arrow_right" class="calendar_arrow" src="resources/chevron-right.svg"></img>
+      </div>
+    </header>
+
+    <!-- calendar_bloc -->
+    <div class="calendar_row" id="calendar_days_row">
+      <div class="calendar_bloc">Lundi</div>
+      <div class="calendar_bloc">Mardi</div>
+      <div class="calendar_bloc">Mercredi</div>
+      <div class="calendar_bloc">Jeudi</div>
+      <div class="calendar_bloc">Vendredi</div>
+      <div class="calendar_bloc">Samedi</div>
+      <div class="calendar_bloc">Dimanche</div>
+    </div>
+    
+    <!-- row 27-2 -->
+    <div class="calendar_row">
+      <div class="calendar_bloc" >27</div>
+      <div class="calendar_bloc">28</div>
+      <div class="calendar_bloc">29</div>
+      <div class="calendar_bloc">30</div>
+      <div class="calendar_bloc">31</div>
+      <div class="calendar_bloc active">1</div>
+      <div class="calendar_bloc active">2</div>
+    </div>
+
+    <!-- row 3-9 -->
+    <div class="calendar_row">
+      <div class="calendar_bloc active">3</div>
+      <div class="calendar_bloc active">4</div>
+      <div class="calendar_bloc active">5</div>
+      <div class="calendar_bloc active">6</div>
+      <div class="calendar_bloc active">7</div>
+      <div class="calendar_bloc active">8</div>
+      <div class="calendar_bloc active">9</div>
+    </div>
+
+    <!-- row 10-16 -->
+    <div class="calendar_row">
+      <div class="calendar_bloc active">10</div>
+      <div class="calendar_bloc active">11</div>
+      <div class="calendar_bloc active">12</div>
+      <div class="calendar_bloc active">13</div>
+      <div class="calendar_bloc active">14</div>
+      <div class="calendar_bloc active">15</div>
+      <div class="calendar_bloc active">16</div>
+    </div>
+
+    <!-- row 17-23 -->
+    <div class="calendar_row">
+      <div class="calendar_bloc active">17</div>
+      <div class="calendar_bloc active">18</div>
+      <div class="calendar_bloc active">19</div>
+      <div class="calendar_bloc active">20</div>
+      <div class="calendar_bloc active">21</div>
+      <div class="calendar_bloc active">22</div>
+      <div class="calendar_bloc active">23</div>
+    </div>
+
+    <!-- row 24-2 -->
+    <div class="calendar_row">
+      <div class="calendar_bloc active">24</div>
+      <div class="calendar_bloc active">25</div>
+      <div class="calendar_bloc active">26</div>
+      <div class="calendar_bloc active">27</div>
+      <div class="calendar_bloc active">28</div>
+      <div class="calendar_bloc">1</div>
+      <div class="calendar_bloc">2</div>
+    </div>
+
+    <!-- row 3-9 -->
+    <div class="calendar_row">
+      <div class="calendar_bloc">3</div>
+      <div class="calendar_bloc">4</div>
+      <div class="calendar_bloc">5</div>
+      <div class="calendar_bloc">6</div>
+      <div class="calendar_bloc">7</div>
+      <div class="calendar_bloc">8</div>
+      <div class="calendar_bloc">9</div>
+    </div>
+  </div>
