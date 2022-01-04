fee2e0f1f5a4ae91e2dbf89a23a9ef1dc3cf601e
Kevin Belzung <belzung.kevin@gmail.com>
2022-01-04 17:35:24 +0100

    calendar: add function to select hours

--- js/calendar.js
+++ js/calendar.js
@@ -9,19 +9,29 @@ var casesCalendar =  document.getElementsByClassName('calendar_bloc_number');
 var dayBlocSelected;
 
 var casesHours =  document.getElementsByClassName('dates_bloc');
-var hoursList = [
+var morningHoursList = [
     '8h','8h15','8h30','8h45',
     '9h','9h15','9h30','9h45',
     '10h','10h15','10h30','10h45',
-    '11h','11h15','11h30','11h45',
+    '11h','11h15','11h30','11h45'
+];
+
+var afternoonHoursList = [
     '14h','14h15','14h30','14h45',
     '15h','15h15','15h30','15h45',
     '16h','16h15','16h30','16h45',
     '17h','17h15','17h30','17h45'
-]
+];
+
+var pageSelected = 1;
+var arrowHours = document.getElementsByClassName('date_bloc_arrow');
+var hourSelected;
+
 
 fillCalendar(currentMonth, currentYear);
 fillHours();
+
+initCalendar();
 
 function fillCalendar(month, year) {
 
@@ -35,7 +45,7 @@ function fillCalendar(month, year) {
     for(let caseHour of casesHours) {
         caseHour.innerHTML = "";
         //caseHour.classList.remove('dates_bloc_active');
-        caseHour.addEventListener("click", hourToggle);
+        caseHour.addEventListener("click", selectHour);
     }
     
     //fill month
@@ -113,13 +123,57 @@ function hourToggle() {
         this.style.color = "";
     }
     else{
-       this.style.backgroundColor = "rgb(39, 96, 168)";
-       this.style.color = "white"; 
+        this.style.backgroundColor = "rgb(39, 96, 168)";
+        this.style.color = "white";
+        unselectHour();
+        hourSelected = this;
+    }
+}
+
+function selectHour() {
+    unselectHour();
+    hourSelected = this;
+    hourSelected.style.backgroundColor = "rgb(39, 96, 168)";
+    hourSelected.style.color = "white";
+}
+
+function unselectHour() {
+    if(hourSelected != null){
+        hourSelected.style.backgroundColor = "";
+        hourSelected.style.color = "";
     }
 }
 
 function fillHours() {
-    for(i=0;i<16;i++) {
-        casesHours[i].innerHTML = hoursList[i];
+    if(pageSelected === 1){        
+        for(i = 0; i < morningHoursList.length; i++) {
+            casesHours[i].innerHTML = morningHoursList[i];
+        }
     }
+    else {
+        for(i = 0; i < afternoonHoursList.length; i++) {
+            casesHours[i].innerHTML = afternoonHoursList[i];
+        }
+    }
+}
+
+function initCalendar() {
+    arrowHours[0].addEventListener("click", nextHours);
+    arrowHours[1].addEventListener("click", previousHours);
+}
+
+function nextHours() {
+    if(pageSelected < 1){
+        pageSelected++;
+        unselectHour();
+    }
+    fillHours();
+}
+
+function previousHours() {
+    if(pageSelected > 0){
+        pageSelected--;
+        unselectHour();
+    }
+    fillHours();
 }
