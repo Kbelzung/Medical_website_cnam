68ae60a400a0d07b227be2c37a9c52053eb8bf8c
Kevin Belzung <belzung.kevin@gmail.com>
2022-01-14 18:02:00 +0100

    create html/css for my_appointment

--- my_appointments.php
+++ my_appointments.php
@@ -7,5 +7,63 @@
     </head>
     <body>
         <?php include('header.php'); ?>
+
+        <section class="main-content">
+            <div class="container">
+                <h1>Mes rendez-vous</h1>
+
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Date</th>
+                            <th>Docteur</th>
+                            <th>Téléphone</th>
+                            <th>Mail</th>
+                            <th>Annuler</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <h6 >06:00 PM</h6>
+                                <small>2 Feb 2021</small>
+                            </td>
+                            <td>
+                                <h6>Dr. Ananth</h6>
+                            </td>
+                            <td>
+                                <h6>+91 9876543215</h6>
+                            </td>
+                            <td>
+                                <h6>Docteur1@mail.com</h6>
+                            </td>
+                            <td>
+                                <button id="validation" type="submit">X</button>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td>
+                                <h6 >06:00 PM</h6>
+                                <small>2 Feb 2021</small>
+                            </td>
+                            <td>
+                                <h6>Dr. Ananth</h6>
+                            </td>
+                            <td>
+                                <h6>+91 9876543215</h6>
+                            </td>
+                            <td>
+                                <h6>Docteur1@mail.com</h6>
+                            </td>
+                            <td>
+                                <button id="validation" type="submit">X</button>
+                            </td>
+                        </tr>
+
+                    </tbody>
+                </table>
+            </div>
+	    </section>
     </body>
 </html>
