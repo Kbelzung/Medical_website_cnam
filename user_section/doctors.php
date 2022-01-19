b8423c8fa1417f46412db2148ef0c4a982c81426
Kevin Belzung <belzung.kevin@gmail.com>
2022-01-19 12:32:53 +0100

    start doctors presentation page

--- user_section/doctors.php
+++ user_section/doctors.php
@@ -3,7 +3,7 @@
 <html>
     <head>
         <title>Mes rendez-vous</title>
-        <link rel="stylesheet" href="css/my_appointments.css">
+        <link rel="stylesheet" href="css/doctors.css">
     </head>
     <body>
     <?php 
@@ -16,6 +16,73 @@
             include('menu_connected.php'); 
         }
     ?>
+
+    <h1> EQUIPE MEDICALE </h1>
+    <div id=container>
+        <div class="row">
+            <div class="element">
+                <img src="\admin_section\photos_doctors\team-image2.jpg"></img>
+                <div class="infos">
+                    <h3>Docteur Strange</h3>
+                    <p>Medecin</p>
+                    <div class="contact-info">
+                        <p><img class="icons" src="resources/phone.svg"></img>0623456789</p>
+                        <p><img class="icons" src="resources/newsletter.svg"></img>doctor.strange@marvel.com</p>
+                    </div>
+                </div>
+            </div>
+            <div class="element">
+                <img src="\admin_section\photos_doctors\team-image2.jpg">
+                <h6>Docteur Strange</h6>
+                <h6>Medecin</h6>
+                <h6>doctor.strange@marvel.com</h6>
+                <h6>0623456789</h6>
+                <a><button>Modifier</button></a>
+                <a><button>Supprimer</button></a>
+            </div>            
+            <div class="element">
+                <img src="\admin_section\photos_doctors\team-image2.jpg">
+                <h6>Docteur Strange</h6>
+                <h6>Medecin</h6>
+                <h6>doctor.strange@marvel.com</h6>
+                <h6>0623456789</h6>
+                <a><button>Modifier</button></a>
+                <a><button>Supprimer</button></a>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="element">
+                <img src="\admin_section\photos_doctors\team-image2.jpg">
+                <h6>Docteur Strange</h6>
+                <h6>Medecin</h6>
+                <h6>doctor.strange@marvel.com</h6>
+                <h6>0623456789</h6>
+                <a><button>Modifier</button></a>
+                <a><button>Supprimer</button></a>
+            </div>
+            <div class="element">
+                <img src="\admin_section\photos_doctors\team-image2.jpg">
+                <h6>Docteur Strange</h6>
+                <h6>Medecin</h6>
+                <h6>doctor.strange@marvel.com</h6>
+                <h6>0623456789</h6>
+                <a><button>Modifier</button></a>
+                <a><button>Supprimer</button></a>
+            </div>            
+            <div class="element">
+                <img src="\admin_section\photos_doctors\team-image2.jpg">
+                <h6>Docteur Strange</h6>
+                <h6>Medecin</h6>
+                <h6>doctor.strange@marvel.com</h6>
+                <h6>0623456789</h6>
+                <a><button>Modifier</button></a>
+                <a><button>Supprimer</button></a>
+            </div>
+        </div>
+    </div>
+
+
     </body>
     <script src="js/my_appointments.js"></script>
 </html>
