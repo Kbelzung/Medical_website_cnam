75d71081f260b0f6f40f240860f43e79cf750abc
Kevin Belzung <belzung.kevin@gmail.com>
2022-01-21 23:47:36 +0100

    finish function to send an email when an user take an appointment

--- PHPMailer/mailConfig.php
+++ PHPMailer/mailConfig.php
@@ -31,3 +31,34 @@
             return $error;
         }
     }
+
+    function smtpmailerPDF($to, $from, $from_name, $subject, $body, $pathPDF) {
+        $mail = new PHPMailer();
+        $mail->IsSMTP();
+        $mail->SMTPAuth = true; 
+
+        $mail->SMTPSecure = 'ssl'; 
+        $mail->Host = 'smtp.gmail.com';
+        $mail->Port = 465;  
+        $mail->Username = 'cabinet.cnam@gmail.com';
+        $mail->Password = 'NL#7ze6vJ*wpJm';   
+
+        $mail->AddAttachment($pathPDF);
+
+        $mail->From="cabinet.cnam@gmail.com";
+        $mail->FromName=$from_name;
+        $mail->Sender=$from;
+        $mail->AddReplyTo($from, $from_name);
+        $mail->Subject = $subject;
+        $mail->Body =  $body;
+        $mail->AddAddress($to);
+        $mail->IsHTML(true);
+        if(!$mail->Send()) {
+                $error ="Please try Later, Error Occured while Processing...";
+                return $error; 
+        }
+        else {
+            $error = "Thanks You !! Your email is sent.";  
+            return $error;
+        }
+    }
--- database_access/appointment_processing.php
+++ database_access/appointment_processing.php
@@ -1,5 +1,6 @@
 <?php 
-    
+    // reference the Dompdf namespace
+    use Dompdf\Dompdf;
     session_start(); // Start session
     require_once 'config.php';
 
@@ -29,6 +30,65 @@
             $stmt= $bdd->prepare($request);
             $stmt->execute([$idDoctor, $_SESSION['id'], $date, $hour,$checkbox_first_appointment, $select_reason, $textarea_note]);
             
+            $id_appointment = $bdd->lastInsertId();
+            
+            $check = $bdd->prepare('SELECT date, time, note, doctor.first_name, doctor.last_name, title, doctor.phone, doctor.email, user.email as user_mail FROM appointment INNER JOIN doctor ON doctor_id = doctor.id INNER JOIN user ON appointment.user_id = user.id WHERE appointment.id = ?');
+            $check->execute(array($id_appointment));
+            $data = $check->fetch();
+            
+            require_once '../dompdf/autoload.inc.php';
+            // instantiate and use the dompdf class
+            $dompdf = new Dompdf();
+            $dompdf->loadHtml('
+            <!DOCTYPE html>
+                <html>
+                    <head>
+                    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+                    <style>
+                        h1 {
+                            text-align: center;
+                            padding-bottom: 50px;
+                        }
+
+                        p {
+                            padding-left: 50px;
+                        }
+                    </style>
+                    </head>
+                    <body>
+                    <h1>Votre rendez-vous</h1>
+
+                    <p>Votre rendez-vous sera le ' . $data["date"] . ' à ' . $data["time"] . ', avec le docteur  ' . $data["first_name"] . ' ' . $data["last_name"] . ', ' . $data["title"] . '.</p>
+                    <p>En cas de problèmes, vous pouvez contacter le Docteur ' . $data["last_name"] . ' au ' . $data["phone"] . ', ou bien sur son mail ' . $data["email"] . '.</p>
+                    </br>
+
+                    <p>Votre observation :</p>
+                    <p>' . $data["note"] . '</p>
+
+                    </body>
+                </html>
+            ');
+
+            // (Optional) Setup the paper size and orientation
+            $dompdf->setPaper('A4', 'portrait');
+
+            // Render the HTML as PDF
+            $dompdf->render();
+
+            $output = $dompdf->output();
+            $pathPDF = '../resources/pdf_appointments/' . $id_appointment . '.pdf';
+            file_put_contents($pathPDF, $output);
+
+            include('../PHPMailer/mailConfig.php');
+
+            $to   = $data['user_mail'];
+            $from = 'cabinet.cnam@gmail.com';
+            $name = 'Cnam';
+            $subj = 'Rendez-vous médical clinique cnam';
+            $msg = 'Veuilez trouver ci-joint votre récapitulatif de rendez-vous';
+
+            $error=smtpmailerPDF($to,$from, $name ,$subj, $msg, $pathPDF);
+
             header('Location: ../my_appointments'); die();
         }else{ header('Location: ../appointment'); die();}
     }else{ header('Location: ../appointment'); die();}
